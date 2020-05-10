61e57ffb16e9bbc28aef9119acd634c48c930992
Tiago Flach <tiagolucas9830@gmail.com>
2020-05-10 00:01:08 -0300

    lamp2.sh update

--- extras/lam2.sh
+++ extras/lam2.sh
@@ -46,7 +46,7 @@ function update {
 
 # --------------------------------------
 
-#update
+update
 
 
 # Build-essential
@@ -56,12 +56,12 @@ function update {
 #sudo apt install expect -y
 
 # Mlocate
-#sudo apt install mlocate -y
+sudo apt install mlocate -y
 
 # NPM
 #sudo apt install npm -y
 
-#update
+update
 
 
 
@@ -76,7 +76,7 @@ NEW_MYSQL_PASSWORD="senha123*A"
 
 MYSQL_ROOT_PASSWORD="@SuperSenhaRoot*098"
 
-PHPMYADMIN_PASSWORD="$MYSQL_ROOT_PASSWORD"
+PHPMYADMIN_PASSWORD="@SuperSenhaPhpMyAdmin*099"
 
 PHPINI="$(locate -l 1 php.ini)"
 
@@ -85,46 +85,229 @@ PHPINI="$(locate -l 1 php.ini)"
 ## ---------------------------------- ##
 ## ------------- Apache ------------- ##
 ## ---------------------------------- ##
-#sudo apt install apache2 -y
+sudo apt install apache2 -y
 
-#sudo ufw allow OpenSSH
-#sudo ufw allow in "Apache Full"
-#sudo ufw enable
-
-
-
-## ---------------------------------- ##
-## -------------- Php --------------- ##
-## ---------------------------------- ##
-#sudo apt install software-properties-common -y
-#sudo add-apt-repository ppa:ondrej/php -y
-#sudo apt-get update
-#sudo apt install php php-cli php-common php-xdebug php-gd php-mbstring php-intl php-xml php-zip php-pear libapache2-mod-php -y
-
-# Display_errors = on
-#sudo sed -i 's/display_errors = Off/display_errors = On/' $PHPINI
-
-# Permissões da pasta html
-#sudo chmod -R 755 /var/www/html/
-#sudo chown -R $USER:$USER /var/www/html/
+sudo ufw allow OpenSSH
+sudo ufw allow in "Apache"
+sudo ufw enable
 
 
 
 ## ---------------------------------- ##
 ## ------------- MySQL -------------- ##
 ## ---------------------------------- ##
-#sudo apt install mysql-server mysql-client php-mysql -y
+sudo apt install mysql-server mysql-client -y
+sudo mysql_secure_installation
 
-#sudo mysql_secure_installation
+
+### Answer
+# --------------------------------------
+
+#
+# Securing the MySQL server deployment.
+#
+# Connecting to MySQL using a blank password.
+#
+# VALIDATE PASSWORD COMPONENT can be used to test passwords
+# and improve security. It checks the strength of password
+# and allows the users to set only those passwords which are
+# secure enough. Would you like to setup VALIDATE PASSWORD component?
+#
+# Press y|Y for Yes, any other key for No:
+#
+### y
+
+# --------------------------------------
+
+# There are three levels of password validation policy:
+#
+# LOW    Length >= 8
+# MEDIUM Length >= 8, numeric, mixed case, and special characters
+# STRONG Length >= 8, numeric, mixed case, special characters and dictionary file
+#
+# Please enter 0 = LOW, 1 = MEDIUM and 2 = STRONG:
+#
+### 1
+
+# --------------------------------------
+
+# Please set the password for root here.
+#
+# New password:
+#
+### $MYSQL_ROOT_PASSWORD
+
+# --------------------------------------
+
+# Re-enter new password:
+#
+### $MYSQL_ROOT_PASSWORD
+
+# --------------------------------------
+
+# Estimated strength of the password: 100 
+# Do you wish to continue with the password provided?(Press y|Y for Yes, any other key for No) :
+#
+### y
+
+# --------------------------------------
+
+# By default, a MySQL installation has an anonymous user,
+# allowing anyone to log into MySQL without having to have
+# a user account created for them. This is intended only for
+# testing, and to make the installation go a bit smoother.
+# You should remove them before moving into a production
+# environment.
+#
+# Remove anonymous users? (Press y|Y for Yes, any other key for No) :
+#
+### y
+
+# --------------------------------------
+
+# Normally, root should only be allowed to connect from
+# 'localhost'. This ensures that someone cannot guess at
+# the root password from the network.
+#
+# Disallow root login remotely? (Press y|Y for Yes, any other key for No) :
+#
+### y
+
+# --------------------------------------
+
+# By default, MySQL comes with a database named 'test' that
+# anyone can access. This is also intended only for testing,
+# and should be removed before moving into a production
+# environment.
+#
+# Remove test database and access to it? (Press y|Y for Yes, any other key for No) :
+#
+### y
+
+# --------------------------------------
+
+#  - Dropping test database...
+# Success.
+#
+#  - Removing privileges on test database...
+# Success.
+#
+# Reloading the privilege tables will ensure that all changes
+# made so far will take effect immediately.
+#
+# Reload privilege tables now? (Press y|Y for Yes, any other key for No) :
+#
+### y
+
+# --------------------------------------
+
+# Success.
+#
+# All done!
+
+# --------------------------------------
+
+sudo ufw enable
+sudo ufw allow mysql
+
+
+
+## ---------------------------------- ##
+## -------------- Php --------------- ##
+## ---------------------------------- ##
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt-get update
+sudo apt install php php-cli php-common php-xdebug php-mysql php-gd php-mbstring php-intl php-xml php-zip php-pear libapache2-mod-php -y
+
+# Display_errors = on
+sudo sed -i 's/display_errors = Off/display_errors = On/' $PHPINI
+
+# Permissões da pasta html
+sudo chmod -R 755 /var/www/html/
+sudo chown -R $USER:$USER /var/www/html/
+
 
 
 
 ## ---------------------------------- ##
 ## ---------- PHP MyAdmin ----------- ##
 ## ---------------------------------- ##
-#update
 
-sudo apt install phpmyadmin php-mbstring -y
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-secure-phpmyadmin-on-ubuntu-20-04
+
+sudo apt install phpmyadmin php-mbstring php-zip php-gd php-json php-curl -y
+
+## Select
+#
+# apache2
+
+# --------------------------------------
+
+# ┌─────────────────────┤ Configurando phpmyadmin ├─────────────────────┐
+# │                                                                     │ 
+# │ O pacote phpmyadmin deve ter um banco de dados instalado e          │ 
+# │ configurado antes de poder ser usado. Isso pode opcionalmente ser   │ 
+# │ tratado com o dbconfig-common.                                      │ 
+# │                                                                     │ 
+# │ Se você é um administrador de bancos de dados avançado e sabe que   │ 
+# │ você quer fazer essa configuração manualmente ou se seu banco de    │ 
+# │ dados já foi instalado e configurado, você deve recusar esta        │ 
+# │ opção. Detalhes sobre o que precisa ser feito devem estar           │ 
+# │ provavelmente disponíveis em /usr/share/doc/phpmyadmin.             │ 
+# │                                                                     │ 
+# │ Se não, você deve provavelmente escolher essa opção.                │ 
+# │                                                                     │ 
+# │ Configurar banco de dados para phpmyadmin com dbconfig-common?      │ 
+# │                                                                     │ 
+# │                  <Sim>                     <Não>                    │ 
+# │                                                                     │ 
+# └─────────────────────────────────────────────────────────────────────┘ 
+#
+### <Sim>
+
+# --------------------------------------
+
+#  ┌───────────────────┤ Configurando phpmyadmin ├────────────────────┐
+#  │ Por favor, forneça uma senha para o phpmyadmin registrar com o   │ 
+#  │ servidor de banco de dados. Se deixada em branco, uma senha      │ 
+#  │ aleatória será gerada.                                           │ 
+#  │                                                                  │ 
+#  │ Senha MySQL da aplicação para o phpmyadmin:                      │ 
+#  │                                                                  │ 
+#  │ ________________________________________________________________ │ 
+#  │                                                                  │ 
+#  │                 <Ok>                     <Cancelar>              │ 
+#  │                                                                  │ 
+#  └──────────────────────────────────────────────────────────────────┘
+#
+### $PHPMYADMIN_PASSWORD
+### <Ok>
+
+# --------------------------------------
+
+#				  ┌────┤ Configurando phpmyadmin ├─────┐
+#                 │                                    │ 
+#                 │                                    │ 
+#                 │ Confirmação de senha:              │ 
+#                 │                                    │ 
+#                 │ __________________________________ │ 
+#                 │                                    │ 
+#                 │       <Ok>           <Cancelar>    │ 
+#                 │                                    │ 
+#                 └────────────────────────────────────┘ 
+#
+### $PHPMYADMIN_PASSWORD
+### <Ok>
+
+# --------------------------------------
+
+
+
+
+
+
+
 sudo phpenmod mbstring
 sudo systemctl restart apache2
 
