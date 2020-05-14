555b175d96bf87915ca7e55a4937685077b8686b
Tiago Flach <tiagolucas9830@gmail.com>
2020-05-14 15:07:01 -0300

    mysql.sh test

--- extras/mysql.sh
+++ extras/mysql.sh
@@ -19,7 +19,7 @@ PHPMYADMIN_PASSWORD="@SuperSenhaPhpMyAdmin*099"
 #sudo mysqladmin -u root password $MYSQL_ROOT_PASSWORD
 
 
-sudo mysql -u root --password=$MYSQL_ROOT_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$MYSQL_ROOT_PASSWORD';"
+#sudo mysql -u root --password=$MYSQL_ROOT_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$MYSQL_ROOT_PASSWORD';"
 
 
 # Working
