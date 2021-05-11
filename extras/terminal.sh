4ee556ac2ef09a184490079e5f63a3893532689b
Tiago Lucas Flach <tiagolucas9830@gmail.com>
2021-05-11 10:46:53 -0300

    zsh plugins update

--- extras/terminal.sh
+++ extras/terminal.sh
@@ -1,11 +1,33 @@
 #!/usr/bin/env bash
 
+# https://drive.google.com/file/d/1lubEpUhOz6RqdYHtrnC6v1EQSW29xnoO/view
+
 ## ----- Prgramas a serem instalados via apt ----- ##
 sudo apt install zsh -y
 chsh -s /bin/zsh
 
 # ---
 sudo apt install curl wget git -y
+
+# Definir ZSH como padrão
+sudo usermod --shell $(which zsh) $USER
+
+# ZSH Highlight
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting 
+
+# Adicione:
+# zsh-syntax-highlighting 
+
+# Em ~/.zshrc (plugins)
+
+# ZSH Suggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+# Adicione:
+# zsh-autosuggestions (plugins)
+
+# Buscador com ctrl+t FZF
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 
 echo -e "Reinicie a sessão"
 
