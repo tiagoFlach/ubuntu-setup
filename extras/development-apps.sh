e952ca7ad1bd94b4efdc569b78de83857c84eb6a
Tiago Lucas Flach <tiagolucas9830@gmail.com>
2022-09-14 17:10:58 -0300

    Update github-cli key

--- extras/development-apps.sh
+++ extras/development-apps.sh
@@ -65,6 +65,7 @@ git config --global user.name "Tiago Lucas Flach"
 # --------------------------------------
 if [ ! -f "/usr/share/keyrings/githubcli-archive-keyring.gpg" ]; then
 	curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+	sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 fi
 
