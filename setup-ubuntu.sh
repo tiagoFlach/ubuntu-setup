1cba6fbb8202cde754d86eca464abf346c900ab6
Tiago Lucas Flach <tiagolucas9830@gmail.com>
2023-04-24 22:44:27 -0300

    Update gstreamer

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -160,13 +160,21 @@ PROGRAMS_APT=(
 	ffmpeg
 	laptop-mode-tools
 	lsb
-	gstreamer1.0-libav
-	gstreamer1.0-plugins-bad
-	gstreamer1.0-plugins-ugly
-	libgstreamer1.0-dev 
-	libgstreamer-plugins-bad1.0-dev 
+	libgstreamer1.0-dev
 	libgstreamer-plugins-base1.0-dev 
-	libgstreamer-plugins-good1.0-dev
+	libgstreamer-plugins-bad1.0-dev 
+	gstreamer1.0-plugins-base 
+	gstreamer1.0-plugins-good 
+	gstreamer1.0-plugins-bad 
+	gstreamer1.0-plugins-ugly 
+	gstreamer1.0-libav 
+	gstreamer1.0-tools 
+	gstreamer1.0-x 
+	gstreamer1.0-alsa 
+	gstreamer1.0-gl
+	gstreamer1.0-gtk3 
+	gstreamer1.0-qt5 
+	gstreamer1.0-pulseaudio
 	net-tools
 	rar
 	ubuntu-restricted-extras
