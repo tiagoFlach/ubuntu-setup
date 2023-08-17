905a67be57cd02cbd8722c3fa25acad346bd5b3d
Tiago Lucas Flach <tiagolucas9830@gmail.com>
2023-08-17 19:03:13 -0300

    Add ufrgs vpn

--- extras/vpn.sh
+++ extras/vpn.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Madesa
+# --------------------------------------------------
+
 # Import
 sudo nmcli connection import type openvpn file ~/Documentos/Madesa/Acesso\ Remoto/t.flach@mds.local_ssl_vpn_config.ovpn
 
@@ -10,6 +13,23 @@ sudo nmcli connection modify t.flach@mds +vpn.data "username=t.flach"
 # Configs
 sudo nmcli connection modify t.flach@mds connection.permissions user:$USER
 sudo nmcli connection modify t.flach@mds ipv4.never-default true
+
+# Remmina
+sed -i 's/disable_tray_icon=false/disable_tray_icon=true/' "~/.config/remmina/remmina.pref"
+
+# UFRGS
+# --------------------------------------------------
+
+# Import
+sudo nmcli connection import type openvpn file ~/Documentos/UFRGS/VPN/VPN-UFRGS.ovpn
+
+# User
+sudo nmcli connection modify VPN-UFRGS connection.id UFRGS
+sudo nmcli connection modify UFRGS +vpn.data "username=00275896"
+
+# Configs
+sudo nmcli connection modify UFRGS connection.permissions user:$USER
+sudo nmcli connection modify UFRGS ipv4.never-default true
 
 # Remmina
 sed -i 's/disable_tray_icon=false/disable_tray_icon=true/' "~/.config/remmina/remmina.pref"
